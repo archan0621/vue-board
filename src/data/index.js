ac6834713154a15b54a713051f2c7fbc97d66031
archan <joshua_0621@naver.com>
2020-11-18 09:00:47 +0900

    20201118
    update data

--- src/data/index.js
+++ src/data/index.js
@@ -10,8 +10,43 @@ export default [
     content: '2'
   },
   {
-    writer :'웬',
-    title : '레드벨벳3',
+    writer: '웬',
+    title: '레드벨벳3',
     content: '3'
+  },
+  {
+    writer: '제이홉',
+    title: '방탄소년단',
+    content: '4'
+  },
+  {
+    writer: '뷔',
+    title: '방탄소년단',
+    content: '4'
+  },
+  {
+    writer: '정국',
+    title: '방탄소년단',
+    content: '5'
+  },
+  {
+    writer: '지민',
+    title: '방탄소년단',
+    content: '6'
+  },
+  {
+    writer: '슈가',
+    title: '방탄소년단',
+    content: '7'
+  },
+  {
+    writer: '진',
+    title: '방탄소년단',
+    content: '8'
+  },
+  {
+    writer: '랩몬스터',
+    title: '방탄소년단',
+    content: '9'
   }
 ]
